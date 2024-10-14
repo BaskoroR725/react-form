950966439ac029f6b9c61153d12deb830d8dc900
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-14 21:38:02 +0700

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UsernameForm from './UsernameForm'
+import SignupForm from './SignUpForm'
+
+function App() {
+  return (
+    <>
+      <UsernameForm/>
+      <SignupForm />
+    </>
+  )
+}
+
+export default App
